5fb9d380f21433a31d5c9ae96d7189ff92e9a876
PanderMusubi <pander@users.sourceforge.net>
2020-04-29 13:46:04 +0200

    minor

--- scripts/3-compare.sh
+++ scripts/3-compare.sh
@@ -5,17 +5,17 @@ if [ ! -e comparisons ]; then
 	mkdir comparisons
 fi
 
-# remove lines with only comments and empty lines
-grep -v ^# nl.aff | grep -v ^$ > comparisons/new.aff
-grep -v ^# downloads/nl.aff | grep -v ^$ > comparisons/prv.aff
+grep -v ^# nl.aff | sed -e 's/\s#.*//g' | sed -e '/^\s*$/d' > comparisons/new.aff
+grep -v ^# downloads/nl.aff | sed -e 's/\s#.*//g' | sed -e '/^\s*$/d' > comparisons/prv.aff
 
 # omit first line with total number of words
-#tail -n +2 nl.dic | sort > comparisons/new.dic
-#tail -n +2 downloads/nl.dic | sort > comparisons/prv.dic
+tail -n +2 nl.dic | sort > comparisons/new.dic
+tail -n +2 downloads/nl.dic | sort > comparisons/prv.dic
 
 cd comparisons
 diff -ub prv.aff new.aff > aff_prv_new.diff
-diff2html -s side -o stdout -i file -- aff_prv_new.diff > aff_prv_new.html
+# next lines open a web browser!
+diff2html -s side -F aff_prv_new.html -i file -- aff_prv_new.diff &
 
 #TODO only later with less differences, too heavy now
 #diff -ub prv.dic new.dic > dic_prv_new.diff
